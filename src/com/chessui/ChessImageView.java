fa223e8602eb0c414e892d0b176bd2d273644027
AndrewXU <zlqxccxuy@gmail.com>
2013-04-16 23:15:16 +0800

    ChessImageView.java
    Signed-off-by: AndrewXU <zlqxccxuy@gmail.com>

---
+++ src/com/chessui/ChessImageView.java
@@ -0,0 +1,22 @@
+package com.chessui;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
+
+public class ChessImageView extends ImageView{
+
+	public ChessImageView(Context context) {
+		super(context);
+	}
+
+	public class OnChessImageViewClickListener implements OnClickListener{
+
+		@Override
+		public void onClick(View v) {
+			// XXX Auto-generated method stub
+			
+		}
+		
+	}
+}
